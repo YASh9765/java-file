d07f08f691c72627dec6b3396ba3c6225f41f188
YASh9765 <yashbijo87@gmail.com>
2025-08-07 22:57:15 +0530

    inheritance

--- inheritance/VehicleDetails.java
+++ inheritance/VehicleDetails.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.time.Year;
 
-//class Vehicle {
+class Vehicle {
     String brand;
     String model;
     int year;
@@ -142,3 +142,4 @@ public class VehicleDetails {
     }
 
 }  
+
